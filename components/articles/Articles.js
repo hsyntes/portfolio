c0a24708aea7cd5729a60854210e06a44a1ed3f5
hsyntes <se.hsyntes@gmail.com>
2023-09-14 04:51:47 +0300

    update articles

--- components/articles/Articles.js
+++ components/articles/Articles.js
@@ -1,7 +1,17 @@
-import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Card from "../ui/Card";
+import Button from "../ui/Button";
 
 const Articles = ({ icons, articles }) => {
+  console.log(articles);
+
+  const [articledHovered, setArticleHovered] = useState(false);
+
   return (
     <section id="articles" className="my-20 lg:my-40">
       <h6 className="font-bold text-2xl lg:text-4xl text-center">
@@ -40,40 +50,71 @@ const Articles = ({ icons, articles }) => {
               {icon.icon_name === "develop" &&
                 "Building real-world Full Stack web apps"}
             </p>
-            {/* <li className="text-center my-12 lg:my-24">
-              <Image
-                src={icons[0].icon_link}
-                className="mx-auto mb-4"
-                width={64}
-                height={64}
-                alt={icons[0].icon_name}
-              />
-              <span className="text-2xl">Ticks & Trips</span>
-            </li> */}
           </motion.li>
         ))}
       </ul>
-      <ul>
+      <ul className="grid grid-cols-12 gap-4">
         {articles?.map((article) => (
           <li
-            className="grid grid-cols-12 gap-3 lg:gap-6 rounded hover:bg-white hover:dark:bg-dark-darker hover:shadow p-4 my-12 lg:my-24 transition"
             key={article._id}
+            className="col-span-6 lg:col-span-4 rounded overflow-hidden"
           >
-            <Image
-              src={article.article_thumbnail}
-              width={1080}
-              height={1350}
-              className="col-span-3 lg:col-span-2 rounded"
-              alt={article.article_title}
-            />
-            <section className="col-span-9 lg:col-span-10">
-              <h1 className="font-bold text-2xl lg:text-3xl mb-2">
-                {article.article_title}
-              </h1>
-              <p className="text-gray-500 dark:text-gray-400 text-sm lg:text-base">
-                {article.article_description}
-              </p>
-            </section>
+            <Link
+              href="https://github.com/hsyntes"
+              className="group relative overflow-hidden"
+              onMouseEnter={() => setArticleHovered(true)}
+              onMouseLeave={() => setArticleHovered(false)}
+            >
+              <Image
+                src={article.article_thumbnail}
+                width={1080}
+                height={1350}
+                className="rounded border group-hover:opacity-75 group-hover:dark:opacity-40 transition"
+                alt={article.article_title}
+                priority={true}
+              />
+              <motion.div className="hidden lg:block absolute w-full h-96 top-full -translate-y-12 left-0 backdrop-blur group-hover:-translate-y-full transition-all duration-200">
+                <Card.Header className="my-3">
+                  <motion.div
+                    animate={
+                      articledHovered ? { rotate: [0, 180] } : { rotate: 0 }
+                    }
+                    transition={{ ease: "easeOut", duration: 0.5 }}
+                    className="mx-auto"
+                  >
+                    <motion.div
+                      animate={!articledHovered ? { y: [0, 5, 0] } : { y: [0] }}
+                      transition={{
+                        ease: "easeInOut",
+                        duration: 1.5,
+                        repeat: Infinity,
+                      }}
+                    >
+                      <FontAwesomeIcon
+                        icon={faAngleUp}
+                        size="xl"
+                        className={
+                          articledHovered ? "dark:text-white" : "text-dark"
+                        }
+                      />
+                    </motion.div>
+                  </motion.div>
+                </Card.Header>
+                <Card.Body className="px-6">
+                  <h1 className="font-bold text-lg text-center mb-3">
+                    {article.article_title}
+                  </h1>
+                  <p className="text-justify">{article.article_description}</p>
+                  <Button
+                    type="button"
+                    variant="light"
+                    className="absolute bottom-6 left-1/2 -translate-x-1/2 !text-xs"
+                  >
+                    Read More
+                  </Button>
+                </Card.Body>
+              </motion.div>
+            </Link>
           </li>
         ))}
       </ul>
