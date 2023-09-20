5b98c0ce7172b62a4b2f71cd2e27b10a1008f548
hsyntes <se.hsyntes@gmail.com>
2023-09-20 05:31:50 +0300

    update input ui

--- components/ui/Input.js
+++ components/ui/Input.js
@@ -5,7 +5,7 @@ const Input = ({ type, name, placeholder, value, onChange, onBlur }) => (
       id={name}
       name={name}
       placeholder={placeholder}
-      className="form-input peer w-full bg-light lg:bg-white dark:bg-black lg:dark:bg-dark border-0 border-b-2 focus:border-b-primary placeholder:text-light placeholder:lg:text-white placeholder:lg:dark:text-dark placeholder:dark:text-black focus:ring-0 ps-0"
+      className="form-input peer w-full bg-light lg:bg-white dark:bg-black lg:dark:bg-dark border-0 border-b border-b-gray-400 dark:border-b-gray-600 focus:border-b-primary placeholder:text-light placeholder:lg:text-white placeholder:lg:dark:text-dark placeholder:dark:text-black focus:ring-0 ps-0"
       value={value}
       onChange={onChange}
       onBlur={onBlur}
