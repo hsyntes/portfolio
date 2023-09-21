d165426a3f8b756e8f36344f96d606ccb5d338a2
hsyntes <se.hsyntes@gmail.com>
2023-09-21 08:29:27 +0300

    update articles ui

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,8 +1,10 @@
 import Container from "@/components/container/Container";
 import MernText from "@/components/mern-text/MernText";
+import fetchData from "@/utils/fetchData";
 import Head from "next/head";
+import Image from "next/image";
 
-const ArticlesPage = () => {
+const ArticlesPage = ({ icons }) => {
   return (
     <>
       <Head>
@@ -12,23 +14,66 @@ const ArticlesPage = () => {
         </title>
       </Head>
       <header
+        className="flex flex-col w-full items-center justify-center"
         style={{
-          height: "35vh",
+          height:
+            typeof window !== "undefined" && window.innerWidth >= 922
+              ? "35vh"
+              : "20vh",
           backgroundImage: `url("${process.env.NEXT_PUBLIC_AWS_S3_BUCKET}/banner.png")`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
       >
-        <h1 className="order-last lg:order-first font-bold text-2xl lg:text-5xl !text-dark text-center">
+        <h1 className="font-bold text-2xl lg:text-5xl !text-dark text-center mb-2">
           LEARN
           <br />
           <MernText />
           &nbsp;DEVELOPMENT
         </h1>
+        <section className="grid grid-cols-12 gap-3 lg:gap-0 lg:w-1/3">
+          <section className="flex items-center col-span-4">
+            <Image
+              src={icons[0].icon_link}
+              width={24}
+              height={24}
+              alt={icons[0].icon_name}
+            />
+            <p className="text-gray-600 ms-1">Tricks & Tips</p>
+          </section>
+          <section className="flex items-center justify-center col-span-4 my-1 lg:my-0">
+            <Image
+              src={icons[1].icon_link}
+              width={24}
+              height={24}
+              alt={icons[1].icon_name}
+            />
+            <p className="text-gray-600 ms-1">Experience</p>
+          </section>
+          <section className="flex items-center col-span-4 justify-end">
+            <Image
+              src={icons[2].icon_link}
+              width={24}
+              height={24}
+              alt={icons[2].icon_name}
+            />
+            <p className="text-gray-600 ms-1">Real Apps</p>
+          </section>
+        </section>
       </header>
       <Container className="my-20"></Container>
     </>
   );
 };
+
+export async function getServerSideProps() {
+  const iconsData = await fetchData("icons");
+
+  return {
+    props: {
+      icons: iconsData.data.icons,
+    },
+  };
+}
 
 export default ArticlesPage;
