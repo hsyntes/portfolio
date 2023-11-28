7bfcc170f8d22e250c024050a2e1bd68efde0381
hsyntes <se.hsyntes@gmail.com>
2023-11-29 01:46:25 +0200

    dark/light theme based on users' preference

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import Links from "./links/Links";
 // * Root layout
 const Layout = ({ children }) => (
   <>
-    <header className="bg-light dark:bg-black sticky top-0 py-2 lg:py-4 z-10">
+    <header className="sticky top-0 py-2 lg:py-4 z-10">
       <Navbar />
     </header>
     <main>
